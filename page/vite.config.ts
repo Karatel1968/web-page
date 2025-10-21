5fcae02c65d3b15bd4b896a5ec6c6c307fb2d6ed
Oleg Urin <olegurin76@mail.ru>
2025-10-21 22:01:54 +0300

    fixed vite config

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Слушаем все интерфейсы
+    port: 5173,
+    strictPort: true, // Запрещаем автоматический выбор порта
+    hmr: {
+      clientPort: 5173, // Важно для HMR в Docker
+    },
+    // Разрешаем CORS для внешних запросов
+    cors: true
+  }
 })
